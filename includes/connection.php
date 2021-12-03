3340f83269f46f16d2988fcdcaad217b895b5073
= <=>
2021-12-03 09:58:00 +0700

    new

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,3 @@
 <?php
- $db = mysqli_connect('10.0.0.127', 'admin', 'admin@Nicken5') or
-        die ('Unable to connect. Check your connection parameters.');
-        mysqli_select_db($db, 'erp3' ) or die(mysqli_error($db));
+ $db = mysqli_connect("172.20.0.0", "root", "", "erp3");
 ?>
